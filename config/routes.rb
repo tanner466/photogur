ec31e5a7af34e2f711bf014fef73b014b6c93e7b
tanner466 <tanner466@gmail.com>
2016-01-25 14:24:52 -0500

    Configureing root as pictures

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   post 'pictures' => 'pictures#create' # this is a new line of code
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
-  
+
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  root 'pictures#index'
+  get 'pictures' => 'pictures#index'
 
 
 
