e5a283343476750e28ac75bdb8cebd38d8816c3e
tanner466 <tanner466@gmail.com>
2016-01-25 14:30:00 -0500

    Adding feature to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+
 
 
 
