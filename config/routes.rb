6eec24ffa4e024e9eaefdf9781e3145e3f28045f
tanner466 <tanner466@gmail.com>
2016-01-25 16:22:58 -0500

    Pictures work as links to edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => 'pictures#create' # this is a new line of code
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-
-  get 'pictures' => 'pictures#index'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
-
   root 'pictures#index'
+
+  resources :pictures
 
 
 
